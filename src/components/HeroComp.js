62ab722eb53ba1dfa2155f8ec46b7444c8ffb5c1
rsyahbudin <syahbudin45@gmail.com>
2023-09-22 00:44:01 +0700

    install animation and animate hero section

--- src/components/HeroComp.js
+++ src/components/HeroComp.js
@@ -6,10 +6,15 @@ const HeroComp = () => {
       <div className="hero min-vh-100 w-100" id="home">
          <Container>
             <Row>
-                <Col>
-                    <h1 className="text-white text-center fs-1">Selamat Datang di Elevasi Kontraktor</h1>
-                    <p className="text-white text-center text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione repudiandae maxime soluta explicabo commodi.</p>
-                </Col>
+               <Col>
+                  <h1 className="text-white text-center fs-1 animate__animated animate__fadeInUp">
+                     Selamat Datang di Elevasi Kontraktor
+                  </h1>
+                  <p className="text-white text-center text-white-50 animate__animated animate__fadeInUp animate__delay-1s">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Ipsum ratione repudiandae maxime soluta explicabo commodi.
+                  </p>
+               </Col>
             </Row>
          </Container>
       </div>
