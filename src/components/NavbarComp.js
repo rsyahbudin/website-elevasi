8673b9c0869a9f45e09c835fb8074399a7ee8fc9
rsyahbudin <syahbudin45@gmail.com>
2023-12-11 19:49:21 +0700

    update service

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,55 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import Elevasi from "../assets/img/elevasi.png";
+import Logo1 from "../assets/img/logo1.png";
 
 const NavbarComp = () => {
-   const [changeColor, setChangeColor] = useState(false);
+  const [changeColor, setChangeColor] = useState(false);
 
-   const changeBackgroundColor = () => {
-      if (window.scrollY > 680) {
-         setChangeColor(true);
-      } else {
-         setChangeColor(false);
-      }
-   };
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 680) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
 
-   useEffect(() => {
-      changeBackgroundColor();
+  useEffect(() => {
+    changeBackgroundColor();
 
-      window.addEventListener("scroll", changeBackgroundColor);
-   });
+    window.addEventListener("scroll", changeBackgroundColor);
+  });
 
-   return (
-      <div className="sticky-top">
-         <Navbar
-            expand="lg"
-            variant="dark"
-            className={changeColor ? "color-active" : ""}
-         >
-            <Container>
-               <Navbar.Brand href="#home" className="fw-bold fs-4">
-                  Elevasi
-               </Navbar.Brand>
-               <Navbar.Toggle aria-controls="basic-navbar-nav" />
-               <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="ms-auto">
-                     <Nav.Link href="#home" className="mx-2">
-                        Home
-                     </Nav.Link>
-                     <Nav.Link href="#gallery" className="mx-2">
-                        Gallery
-                     </Nav.Link>
-                     <Nav.Link href="#service" className="mx-2">
-                        Services
-                     </Nav.Link>
-                     <Nav.Link href="#faq" className="mx-2">
-                        FAQ
-                     </Nav.Link>
-                  </Nav>
-               </Navbar.Collapse>
-            </Container>
-         </Navbar>
-      </div>
-   );
+  return (
+    <div className="sticky-top">
+      <Navbar
+        expand="lg"
+        variant="dark"
+        className={changeColor ? "color-active" : ""}
+      >
+        <Container>
+          <Navbar.Brand href="#home" className="fw-bold fs-4">
+            <img
+              alt="Logo Elevasi"
+              src={Logo1}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+            />{" "}
+            <img
+              alt="Logo Elevasi"
+              src={Elevasi}
+              width="100"
+              height="50"
+              className="d-inline-block align-top"
+            />{" "}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className="mx-2">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#gallery" className="mx-2">
+                Gallery
+              </Nav.Link>
+              <Nav.Link href="#service" className="mx-2">
+                Services
+              </Nav.Link>
+              <Nav.Link href="#faq" className="mx-2">
+                FAQ
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default NavbarComp;
--- src/components/ServiceComp.js
+++ src/components/ServiceComp.js
@@ -2,65 +2,61 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const ServiceComp = () => {
-   return (
-      <div
-         className="services min-vh-100 d-flex align-items-center"
-         id="service"
-      >
-         <Container>
-            <Row className="mb-5">
-               <Col>
-                  <h1 className="text-center fw-bold" data-aos="fade-up">
-                     Services
-                  </h1>
-                  <p
-                     className="text-center"
-                     data-aos="fade-up"
-                     data-aos-delay="200"
-                  >
-                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                     Cumque, magni.
-                  </p>
-               </Col>
-            </Row>
-            <Row className="row-cols-lg-3 row-cols-md-2 row-cols-1 justify-content-center">
-               <Col
-                  className="text-center py-5 px-3"
-                  data-aos="fade-up"
-                  data-aos-delay="400"
-               >
-                  <i class="fa-solid fa-coins fs-2 mb-3"></i>
-                  <h5 className="fw-bold">Harga Terjangkau</h5>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
-               </Col>
-               <Col
-                  className="text-center py-5 px-3"
-                  data-aos="fade-up"
-                  data-aos-delay="600"
-               >
-                  <i class="fa-solid fa-coins fs-2 mb-3 px-3 "></i>
-                  <h5 className="fw-bold">Harga Terjangkau</h5>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
-               </Col>
-               <Col
-                  className="text-center py-5 px-3"
-                  data-aos="fade-up"
-                  data-aos-delay="800"
-               >
-                  <i class="fa-solid fa-coins fs-2 mb-3 px-3 "></i>
-                  <h5 className="fw-bold">Harga Terjangkau</h5>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
-               </Col>
-            </Row>
-         </Container>
-      </div>
-   );
+  return (
+    <div className="services min-vh-100 d-flex align-items-center" id="service">
+      <Container>
+        <Row className="mb-5">
+          <Col>
+            <h1 className="text-center fw-bold" data-aos="fade-up">
+              Services
+            </h1>
+            <p className="text-center" data-aos="fade-up" data-aos-delay="200">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque,
+              magni.
+            </p>
+          </Col>
+        </Row>
+        <Row className="row-cols-lg-4 row-cols-md-2 row-cols-1 justify-content-center">
+          <Col
+            className="text-center py-5 px-3"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <i class="fa-solid fa-person-digging fs-2 mb-3 px-3 "></i>
+            <h5 className="fw-bold">Contractor</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Col>
+          <Col
+            className="text-center py-5 px-3"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          >
+            <i class="fa-solid fa-house-chimney-window fs-2 mb-3 px-3 "></i>
+            <h5 className="fw-bold">Design 3D</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Col>
+          <Col
+            className="text-center py-5 px-3"
+            data-aos="fade-up"
+            data-aos-delay="800"
+          >
+            <i class="fa-solid fa-paint-roller fs-2 mb-3 px-3"></i>
+            <h5 className="fw-bold">Interior</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Col>
+          <Col
+            className="text-center py-5 px-3"
+            data-aos="fade-up"
+            data-aos-delay="1000"
+          >
+            <i class="fa-solid fa-couch fs-2 mb-3 px-3"></i>
+            <h5 className="fw-bold">Furniture</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ServiceComp;
